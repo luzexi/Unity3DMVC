bd5bf79db2c89166afa41691281d0d05579ee719
luzexi <jesse_luzexi@163.com>
2014-07-16 22:07:24 +0800

    加强控制器
    加强gui基类
    增加scene基类

---
+++ Unity3DMVC/Unity3DMVC/Controller/CScene.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//	BaseScene.cs
+//	Author: Lu Zexi
+//	2014-07-16
+
+ 
+/// <summary>
+/// Base scene.
+/// </summary>
+public class CScene
+{
+	protected static CScene s_cCurrentScene;
+
+	public static void Switch<T>()
+		where T : CScene , new()
+	{
+		if(s_cCurrentScene != null )
+			s_cCurrentScene.OnExit();
+		s_cCurrentScene = new T();
+		s_cCurrentScene.OnEnter();
+	}
+
+	/// <summary>
+	/// Raises the enter event.
+	/// </summary>
+	public virtual void OnEnter()
+	{
+		//
+	}
+
+	/// <summary>
+	/// Raises the exit event.
+	/// </summary>
+	public virtual void OnExit()
+	{
+		//
+	}
+}
--- Unity3DMVC/Unity3DMVC/Controller/UIControllerBase.cs
+++ Unity3DMVC/Unity3DMVC/Controller/UIControllerBase.cs
@@ -1,102 +1,132 @@
-﻿using UnityEngine;
-using System;
-using System.Collections.Generic;
-
-//using BUTTON_CALLBACK = System.Action<>
-
-
-//  UIControllerBase.cs
-//  Author: Lu Zexi
-//  2014-07-05
-
-
-namespace Game.MVC
-{
-    /// <summary>
-    /// the ui controller.
-    /// </summary>
-    public class UIControllerBase : MonoBehaviour
-    {
-        public virtual void Show() { }
-        public virtual void Hiden() { }
-
-        /// <summary>
-        /// find the gameobject.
-        /// </summary>
-        /// <param name="path"></param>
-        /// <returns></returns>
-        public GameObject FIND(string path)
-        {
-            return this.transform.Find(path).gameObject;
-        }
-
-        /// <summary>
-        /// find the mono scripts.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="path"></param>
-        /// <returns></returns>
-        public T FIND<T>(string path) where T : MonoBehaviour
-        {
-            return this.transform.Find(path).GetComponent<T>();
-        }
-
-        /// <summary>
-        /// find the gameobject.
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <param name="path"></param>
-        /// <returns></returns>
-        public GameObject FIND(GameObject obj, string path)
-        {
-            return obj.transform.Find(path).gameObject;
-        }
-
-        /// <summary>
-        /// find the mono scripts
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="obj"></param>
-        /// <param name="path"></param>
-        /// <returns></returns>
-        public T FIND<T>(GameObject obj, string path) where T : MonoBehaviour
-        {
-            return obj.transform.Find(path).GetComponent<T>();
-        }
-
-        /// <summary>
-        /// regist the mono event.
-        /// </summary>
-        /// <param name="mono"></param>
-        /// <param name="callback"></param>
-        /// <param name="arg"></param>
-        public void RegistEvent(MonoBehaviour mono, UIEvent.OnCallBack callback, params object[] arg)
-        {
-            Regist(mono.gameObject, callback, arg);
-        }
-
-        /// <summary>
-        /// regist the gameobject event.
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <param name="callback"></param>
-        /// <param name="arg"></param>
-        public void RegistEvent(GameObject obj, UIEvent.OnCallBack callback, params object[] arg)
-        {
-            Regist(obj, callback, arg);
-        }
-
-        /// <summary>
-        /// regist the event .
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <param name="callback"></param>
-        /// <param name="arg"></param>
-        protected void Regist(GameObject obj, UIEvent.OnCallBack callback, object[] arg)
-        {
-            UIEvent uiEvent = obj.AddComponent<UIEvent>();
-            uiEvent.m_cEvent = callback;
-            uiEvent.m_vecArg = arg;
-        }
-    }
-}
+﻿//using UnityEngine;
+//using System;
+//using System.Collections.Generic;
+//
+////using BUTTON_CALLBACK = System.Action<>
+//
+//
+////  UIControllerBase.cs
+////  Author: Lu Zexi
+////  2014-07-05
+//
+//
+//namespace Game.MVC
+//{
+//    /// <summary>
+//    /// the ui controller.
+//    /// </summary>
+//    public class UIControllerBase<T> : MonoBehaviour
+//		where T : MonoBehaviour
+//    {
+//		protected static T s_cInstance;
+//
+//		public static T sInstance
+//		{
+//			get
+//			{
+//				if(s_cInstance == null)
+//				{
+//					Type t = typeof(T);
+//					GameObject obj = new GameObject(t.Name);
+//					s_cInstance = obj.AddComponent<T>();
+//				}
+//				return s_cInstance;
+//			}
+//		}
+//
+//		/// <summary>
+//		/// Show this instance.
+//		/// </summary>
+//        public virtual void Show()
+//		{
+//			//
+//		}
+//
+//		/// <summary>
+//		/// Hiden this instance.
+//		/// </summary>
+//		public virtual void Hiden()
+//		{
+//			//
+//		}
+//
+//        /// <summary>
+//        /// find the gameobject.
+//        /// </summary>
+//        /// <param name="path"></param>
+//        /// <returns></returns>
+//        public GameObject FIND(string path)
+//        {
+//            return this.transform.Find(path).gameObject;
+//        }
+//
+//        /// <summary>
+//        /// find the mono scripts.
+//        /// </summary>
+//        /// <typeparam name="T"></typeparam>
+//        /// <param name="path"></param>
+//        /// <returns></returns>
+//        public T FIND<T>(string path) where T : MonoBehaviour
+//        {
+//            return this.transform.Find(path).GetComponent<T>();
+//        }
+//
+//        /// <summary>
+//        /// find the gameobject.
+//        /// </summary>
+//        /// <param name="obj"></param>
+//        /// <param name="path"></param>
+//        /// <returns></returns>
+//        public GameObject FIND(GameObject obj, string path)
+//        {
+//            return obj.transform.Find(path).gameObject;
+//        }
+//
+//        /// <summary>
+//        /// find the mono scripts
+//        /// </summary>
+//        /// <typeparam name="T"></typeparam>
+//        /// <param name="obj"></param>
+//        /// <param name="path"></param>
+//        /// <returns></returns>
+//        public T FIND<T>(GameObject obj, string path) where T : MonoBehaviour
+//        {
+//            return obj.transform.Find(path).GetComponent<T>();
+//        }
+//
+//        /// <summary>
+//        /// regist the mono event.
+//        /// </summary>
+//        /// <param name="mono"></param>
+//        /// <param name="callback"></param>
+//        /// <param name="arg"></param>
+//        public void RegistEvent(MonoBehaviour mono, UIEvent.OnCallBack callback, params object[] arg)
+//        {
+//            Regist(mono.gameObject, callback, arg);
+//        }
+//
+//        /// <summary>
+//        /// regist the gameobject event.
+//        /// </summary>
+//        /// <param name="obj"></param>
+//        /// <param name="callback"></param>
+//        /// <param name="arg"></param>
+//        public void RegistEvent(GameObject obj, UIEvent.OnCallBack callback, params object[] arg)
+//        {
+//            Regist(obj, callback, arg);
+//        }
+//
+//        /// <summary>
+//        /// regist the event .
+//        /// </summary>
+//        /// <param name="obj"></param>
+//        /// <param name="callback"></param>
+//        /// <param name="arg"></param>
+//        protected void Regist(GameObject obj, UIEvent.OnCallBack callback, object[] arg)
+//        {
+//            UIEvent uiEvent = obj.AddComponent<UIEvent>();
+//            uiEvent.m_cEvent = callback;
+//            uiEvent.m_vecArg = arg;
+//        }
+//    }
+//}
