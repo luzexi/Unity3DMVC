66a57d3021dd9837828d4c25ae29910ebf427161
luzexi <jesse_luzexi@163.com>
2014-07-18 17:55:34 +0800

    加强view的变量映射
    加强view的变量映射

--- Unity3DMVC/Unity3DMVC/View/UIViewBase.cs
+++ Unity3DMVC/Unity3DMVC/View/UIViewBase.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,11 +12,52 @@ using UnityEngine;
 
 namespace Game.MVC
 {
-    /// <summary>
-    /// The View Base
-    /// </summary>
-    public class UIViewBase : MonoBehaviour
-    {
-        //
-    }
+	/// <summary>
+	/// The View Base
+	/// </summary>
+	public class UIViewBase<T> : MonoBehaviour
+	{
+		void Awake()
+		{
+			Type t = GetType();
+			FieldInfo[] fis = t.GetFields(BindingFlags.Public | BindingFlags.Instance);
+			foreach (FieldInfo f in fis)
+			{
+				Type fieldType = f.FieldType;
+				Transform tra = FIND_CHILD(this.transform , f.Name);
+				if(tra != null)
+				{
+					if( fieldType == typeof(GameObject) )
+					{
+						var com = tra.gameObject;
+						f.SetValue(this,com);
+					}
+					else
+					{
+						var com = tra.GetComponent(fieldType);
+						f.SetValue(this,com);
+					}
+				}
+			}
+		}
+		
+		
+		private Transform FIND_CHILD( Transform parent , string childName )
+		{
+			foreach( Transform item in parent )
+			{
+				if(item.name == childName)
+				{
+					return item;
+				}
+				else
+				{
+					Transform res = FIND_CHILD(item , childName);
+					if(res != null )
+						return res;
+				}
+			}
+			return null;
+		}
+	}
 }
