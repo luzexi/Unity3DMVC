b6c5655cf346a1ade53037ca50480b431a1ce87c
luzexi <jesse_luzexi@163.com>
2014-07-18 16:32:08 +0800

    补充销毁时操作
    补充销毁时操作

--- Unity3DMVC/Unity3DMVC/Controller/GUIBase.cs
+++ Unity3DMVC/Unity3DMVC/Controller/GUIBase.cs
@@ -37,6 +37,14 @@ namespace Game.MVC
 		}
 
 		/// <summary>
+		/// Raises the destroy event.
+		/// </summary>
+		void OnDestroy()
+		{
+			s_cInstance = null;
+		}
+
+		/// <summary>
 		/// Gets a value indicating whether this instance is show.
 		/// </summary>
 		/// <value><c>true</c> if this instance is show; otherwise, <c>false</c>.</value>
